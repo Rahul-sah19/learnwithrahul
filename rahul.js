eae1f96f9465454def6ba6cb0ce92f01e2188376
Rahul Sah <sahrahul123xyz@gmail.com>
2024-08-05 21:21:09 +0545

    new change in js

---
+++ rahul.js
@@ -0,0 +1,9 @@
+let a = 5;
+let b = 6;
+
+let output;
+output= `The sum is : ${ a + b } Rupees`;
+output2= "the sum is : " + (a+b) + " " + "Rupees";
+console.log(output);
+console.log("The sum is :",a+b,"Rupees");
+console.log(output2);
